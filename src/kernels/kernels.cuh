93f79e2721749922be15eb3e233681a1b3aaa3b3
Alan Liddell <alan@vidriotech.com>
2021-02-19 10:01:25 -0500

    Remove extraneous kernels

--- src/kernels/kernels.cuh
+++ src/kernels/kernels.cuh
@@ -1,7 +1,6 @@
 #ifndef RTS_2_SRC_KERNELS_KERNELS_CUH_
 #define RTS_2_SRC_KERNELS_KERNELS_CUH_
 
-#include "ChanAdd.cuh"
 #include "filters.cuh"
 #include "thresholds.cuh"
 
--- tests/kernels_test.cu
+++ tests/kernels_test.cu
@@ -1,60 +1,6 @@
 #include "gtest/gtest.h"
 #include "../src/kernels/kernels.cuh"
 
-TEST(KernelTestSuite, TestSqAdd) {
-  float *x, *y;
-
-  auto N = 1 << 20;
-
-  cudaMallocManaged(&x, N * sizeof(float));
-  cudaMallocManaged(&y, N * sizeof(float));
-
-  for (int i = 0; i < N; i++) {
-    x[i] = 1.0;
-    y[i] = -1.0;
-  }
-
-  int block_size = 256;
-  int nblocks = (N + block_size - 1) / block_size;
-
-  sq_add<<<nblocks, block_size>>>(N, x, y);
-  cudaDeviceSynchronize();
-
-  for (int i = 0; i < N; i++) {
-    EXPECT_EQ(2.0, y[i]);
-  }
-
-  cudaFree(x);
-  cudaFree(y);
-}
-
-TEST(KernelTestSuite, TestSqDiff) {
-  float *x, *y;
-
-  auto N = 1 << 20;
-
-  cudaMallocManaged(&x, N * sizeof(float));
-  cudaMallocManaged(&y, N * sizeof(float));
-
-  for (int i = 0; i < N; i++) {
-    x[i] = 1.0;
-    y[i] = -1.0;
-  }
-
-  int block_size = 256;
-  int nblocks = (N + block_size - 1) / block_size;
-
-  sq_diff<<<nblocks, block_size>>>(N, x, y);
-  cudaDeviceSynchronize();
-
-  for (int i = 0; i < N; i++) {
-    EXPECT_EQ(4.0, y[i]);
-  }
-
-  cudaFree(x);
-  cudaFree(y);
-}
-
 TEST(KernelTestSuite, TestNdiff2KernelShort) {
   auto nchans = 64;
 
